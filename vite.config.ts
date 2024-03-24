e63663b7e4dfa2d2a4dbd0e5f292dc8b0f424d4a
adcGG <979818137@qq.com>
2024-03-24 20:09:24 +0800

    feat: eslint 支持

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import { presetUno, presetAttributify } from 'unocss'
-import { unocssRules, unocssShortcuts } from "./uno.config";
+// import { unocssRules, unocssShortcuts } from "./uno.config";
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
