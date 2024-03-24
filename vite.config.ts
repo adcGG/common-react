085353484e7272f7370df64275999fef20b6352b
adcGG <979818137@qq.com>
2024-03-24 19:56:52 +0800

    feat: 添加unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import UnoCSS from 'unocss/vite'
+import { presetUno, presetAttributify } from 'unocss'
+import { unocssRules, unocssShortcuts } from "./uno.config";
+import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS({
+    // rules: unocssRules,
+    // shortcuts: unocssShortcuts,
+    presets: [
+      presetUno(),
+      presetAttributify(),
+    ],
+    transformers: [
+      transformerAttributifyJsx(), // <--
+    ],
+  })],
 })
